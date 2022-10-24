acdc8370a1099b687a76663aa1906d8a82eca1ee
Quin Henshaw <qhenshaw@gmail.com>
2022-10-24 15:18:57 -0700

    added 2D character assets and 2D hybrid sample

---
+++ Runtime/CharacterSpriteFlipper.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CharacterMovement
+{
+    [RequireComponent(typeof(CharacterMovementBase))]
+    public class CharacterSpriteFlipper : MonoBehaviour
+    {
+        private CharacterMovementBase _movement;
+        private SpriteRenderer _renderer;
+
+        private void Start()
+        {
+            _movement = GetComponent<CharacterMovementBase>();
+            _renderer = GetComponent<SpriteRenderer>();
+        }
+
+        private void Update()
+        {
+            if (!_movement.HasMoveInput) return;
+            _renderer.flipX = _movement.MoveInput.x < 0f;
+        }
+    }
+}
